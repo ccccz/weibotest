fe5a9c6b4f0399d98c43d81abdf26f374fdc9920
GStarP <171250605@smail.nju.edu.cn>
2019-10-01 17:08:33 +0800

    feat: 完成将数据保存到 MongoDB 的 Pipeline

--- weibotest/pipelines.py
+++ weibotest/pipelines.py
@@ -1,11 +1,28 @@
-# -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from weibotest.items import BaseInfoItem
+from weibotest.items import WeiboInfoItem
+from weibotest.settings import MONGO_HOST
+from weibotest.settings import MONGO_PORT
+from weibotest.settings import MONGO_DB_NAME
+import pymongo
 
+class MongoPipeline(object):
 
-class WeibotestPipeline(object):
+    # 初始化数据库连接信息
+    def __init__(self):
+        host = MONGO_HOST
+        port = MONGO_PORT
+        db_name = MONGO_DB_NAME
+        client = pymongo.MongoClient(host, port)
+        self.db = client[db_name]
+        self.base_info = self.db["base_info"]
+
+    # 判断 Item 的类型并存入相应的集合
     def process_item(self, item, spider):
-        return item
+        if isinstance(item, BaseInfoItem):
+            self.base_info.insert(dict(item))
+        elif isinstance(item, WeiboInfoItem):
+            collectionName = item.id + "_weibo_info"
+            self.db[collectionName].insert(dict(item))
+        else:
+            pass
