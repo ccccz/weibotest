51f299bbbd7fd6c1b75ddd316d99495a531065d6
ccccz <171250013@smail.nju.edu.cn>
2019-10-02 16:57:55 +0800

    [feat]完成部分爬虫功能

--- weibotest/items.py
+++ weibotest/items.py
@@ -12,7 +12,10 @@ class WeibotestItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     name = scrapy.Field()  # 大学名称
+    wb = scrapy.Field()   #微博数
     fans = scrapy.Field()  # 粉丝数
     follows = scrapy.Field()  # 关注数
+
+
 
     pass
--- weibotest/spiders/spider.py
+++ weibotest/spiders/spider.py
@@ -29,36 +29,40 @@ class WeiboSpider(scrapy.Spider):
         'SUHB': '04Wu_GeSI9_qSF',
         'SSOLoginState': '1569682984'
     }
+    contTemp=''
 
 
     def parse(self, response):
         #具体爬取内容，目前只有粉丝数可和关注数，微博内容未写
-        filename = (response.url.split("/")[-1]).split("?")[0]
+        #filename = (response.url.split("/")[-1]).split("?")[0]
 
 
         if(self.url_4==2):
-            name = filename
+            name = (response.url.split("/")[-1]).split("?")[0]
+            wb=response.xpath("/html/body/div[3]/div/span/text()").extract()
+            wb=wb[0][3:len(wb[0])-1]
             fans = response.xpath('/ html / body / div[3] / div / a[2] / text()').extract()
             fans=fans[0][3:len(fans[0])-1]
             follows = response.xpath('/ html / body / div[3] / div / a[1] / text()').extract()
             follows=follows[0][3:len(follows[0])-1]
             print(name)
+            print("微博数： "+wb)
             print("粉丝数： "+fans)
             print("关注数： "+follows)
 
 
         url=self.getNextUrl()
         if(len(url)!=0):
-            yield scrapy.Request(url, callback=self.parse, headers=self.headers, cookies=self.cookies)
+            yield scrapy.Request(url, callback=self.sub_parse(), headers=self.headers, cookies=self.cookies)
 
 
 
 
     def start_requests(self):
         #重写了start_requests方法，可以按照自定义顺序爬取界面
-
+        #TODO:暂时修改回调函数，记得改回来self.getNextUrl()
         return [
-            scrapy.Request(self.getNextUrl(), callback=self.parse, headers=self.headers, cookies=self.cookies)
+            scrapy.Request("https://weibo.cn/nju1902?page=2", callback=self.sub_parse, headers=self.headers, cookies=self.cookies)
         ]
 
     def getNextUrl(self):
@@ -77,3 +81,65 @@ class WeiboSpider(scrapy.Spider):
             return url
         else:
             return ""
+
+    def sub_parse(self,response):
+        #// *[ @ id = "M_I8AULotHE"]  /html/body/div[8]/div[1]/span[1]
+        #字段：repost-转发者，content-内容，tag-标签，at-at的人，imgs-配图数，
+        # likeNum-点赞数，repostNum-转发数，commentNum-评论数，time-发博时间
+        for i in range(1,len(response.xpath('/html/body/div[@ class="c"]'))-1):
+
+            repost=response.xpath('/html/body/div[@ class="c"]['+str(i)+']/div[1]/span[1][@ class="cmt"]/a/text()').extract()
+            if(len(repost)==0):    #原创微博
+                repost=''
+                content=''
+                contents=response.xpath('/html/body/div[@ class="c"]['+str(i)+']/div[last()]').css('*::text').extract()
+                contents2=response.xpath('/html/body/div[@ class="c"]['+str(i)+']/div[1]').css('*::text').extract()
+                if contents2[-3]=='收藏':
+                    imgs=0
+                else:
+                    if contents2[-2][0:2]=='组图':
+                        imgs=contents2[-2][3]
+                    else:
+                        imgs=1
+
+                temp=response.xpath('/html/body/div[@ class="c"]['+str(i)+']/div[1]/span/a[last()]')
+                #print((temp.xpath('@href').extract())[0])
+                if (temp.xpath('text()').extract())[0]=='全文':
+                    yield scrapy.Request('https://weibo.cn'+(temp.xpath('@href').extract())[0], callback=self.getContent, headers=self.headers, cookies=self.cookies)
+
+                #print(response.xpath('/html/body/div[@ class="c"]['+str(i)+']/div[1]/span/a[last()]/text()').extract())
+
+                    #yield scrapy.Request(, callback=self.getContent, headers=self.headers, cookies=self.cookies)
+                    #imgs=contents2[-1]
+                   # content = ''
+                #else:
+                 #   content = ''
+                  #  for s in contents2:
+                   #     content = content + s
+                #print(contents)
+                #print(contents2)
+                imgs=0
+            else:      #转发的微博
+                repost=repost[0]
+                contents=response.xpath('/html/body/div[@ class="c"]['+str(i)+']/div[last()]').css('*::text').extract()
+                content=''
+                imgs=0
+                for i in range(1,len(contents)-9):
+                    content=content+contents[i]
+            likeNum=contents[-9][2:len(contents[-9])-1]
+            repostNum=contents[-7][3:len(contents[-7])-1]
+            commentNum=contents[-5][3:len(contents[-5])-1]
+            time=contents[-1]
+            print("转发： " + repost)
+            print("内容： " + content)
+            print("点赞数： "+likeNum)
+            print("转发数： "+repostNum)
+            print("评论数： "+commentNum)
+            print("发博时间： "+time)
+
+            #content=
+    def getContent(self,response):
+        #print(response.body)
+        contents=response.xpath('/html/body/div[@ class="c"][2]/div').css('*::text').extract()
+        #for i in range(4,len(contents)-)
+        print(contents)
